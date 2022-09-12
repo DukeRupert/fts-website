e1f6abf59a36e5df71f6b4d9a18f0d22c799ce43
Logan Williams <logan@firefly.llc>
2022-09-12 14:29:52 -0600

    Added Hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
+import Hero from "$lib/sections/Hero.svelte";
+
     export let data;
     console.log(data)
 </script>
 
-<div class="text-blue-500">
-<h1 class="text-primary-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<img src="/icons/mail.svg" alt="thumbsup" />
-</div>
+<Hero />
