3655405d9b56bb5bbe86fcb1c27563ce5fad0dd7
Logan Williams <logan@firefly.llc>
2022-09-12 19:16:26 -0600

    GoogleMap component created.

---
+++ src/lib/components/GoogleMap.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { COMPANY } from '$lib/constants';
+	import { GenericEmbed } from 'sveltekit-embed';
+
+	export let width = '600';
+	export let height = '450';
+</script>
+
+<GenericEmbed
+	{width}
+	{height}
+	title="Google map"
+	src={COMPANY.mapEmbed}
+	frameborder="0"
+	SameSite="None"
+/>
