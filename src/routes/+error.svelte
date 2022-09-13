e15c81b7be8d1bdde1a9a3bd822c3e0a582becf6
Logan Williams <logan@firefly.llc>
2022-09-12 21:00:13 -0600

    Implented +error.svelte page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<!-- __error.svelte -->
+<div class="min-h-3/4 pt-16 pb-12 flex flex-col bg-white">
+	<main
+		class="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8"
+	>
+		<div class="flex-shrink-0 flex justify-center">
+			<a href="/" class="inline-flex">
+				<span class="sr-only">FtS Excavation</span>
+				<img
+					class="h-36 md:h-48 w-auto p-2 bg-evergreen-500 rounded-md"
+					src="/FtS_Logo.svg"
+					alt="FtS Excavation logo"
+				/>
+			</a>
+		</div>
+		<div class="py-16">
+			<div class="text-center">
+				<p class="text-sm font-semibold text-evergreen-600 uppercase tracking-wide">
+					{$page.status} error
+				</p>
+				<h1 class="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+					{$page.error?.message}
+				</h1>
+				<p class="mt-2 text-base text-gray-500">
+					Sorry, we couldn’t find the page you’re looking for.
+				</p>
+				<div class="mt-6">
+					<a href="/" class="text-base font-medium text-evergreen-600 hover:text-evergreen-500"
+						>Go back home<span aria-hidden="true"> &rarr;</span></a
+					>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
