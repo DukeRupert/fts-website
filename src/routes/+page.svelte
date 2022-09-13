2e369eff1d8393a73ff66ffbf38cea46b15b990c
Logan Williams <logan@firefly.llc>
2022-09-12 23:28:31 -0600

    Added SEO to index route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import type { PageData } from './$types';
+	import SvelteSeo from 'svelte-seo';
 	import AlternatingFeatures from '$lib/sections/AlternatingFeatures.svelte';
 	import GradientFeatures from '$lib/sections/GradientFeatures.svelte';
 	import Hero from '$lib/sections/Hero.svelte';
@@ -10,6 +12,26 @@
 
 	export let data: PageData;
 </script>
+
+<SvelteSeo
+	title="FtS Excavation - Puyallup's excavation experts"
+	description="Professional excavators serving both residential and commercial clients. Services include site prepartion, land development and drainage solutions. Serving the greater Puget Sound region."
+	keywords="excavation land clearing foundations leveling dirt removal residential construction utility installation tacoma puyallup"
+	openGraph={{
+		title: "FtS Excavation - Puyallup's excavation experts",
+		description: `Professional excavators serving both residential and commercial clients. Services include site prepartion, land development and drainage solutions. Serving the greater Puget Sound region.`,
+		url: $page.url.href,
+		type: 'website',
+		images: [
+			{
+				url: `${$page.url.host}/image/P53_900.webp`,
+				width: 960,
+				height: 600,
+				alt: 'FtS Excavation excavator leveling a driveway for a residential customer in Puyallup.'
+			}
+		]
+	}}
+/>
 
 <Hero />
 <AlternatingFeatures />
