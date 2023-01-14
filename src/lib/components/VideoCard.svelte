2f60c57ae565167e93bf6ee494339835d84261bb
Logan Williams <logan@firefly.llc>
2023-01-14 09:51:42 -0700

    Updated Videos styles

---
+++ src/lib/components/VideoCard.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import type { Item } from '$lib/types';
+
+	export let item: Item;
+	const { videoId: id } = item?.id;
+	const { high: thumbnail } = item?.snippet?.thumbnails;
+	const { title, description } = item?.snippet;
+</script>
+
+<div class="h-full card">
+	<header class="card-header">
+		<img
+			src={thumbnail.url}
+			height={thumbnail.height}
+			width={thumbnail.width}
+			class="bg-black/50 w-full aspect-[4/3]"
+			alt="Post"
+		/>
+	</header>
+	<div class="p-4">
+		<div class="space-y-4 text-left">
+			<h3>{title}</h3>
+			{#if description}
+				<article>
+					<p>{description}</p>
+				</article>
+			{/if}
+		</div>
+	</div>
+	<footer>
+		<a href="https://www.youtube.com/watch?v={id}" class="btn btn-outline-primary no-underline mb-4"
+			><span>Watch</span><span>></span></a
+		>
+	</footer>
+</div>
--- src/lib/sections/ServiceArea.svelte
+++ src/lib/sections/ServiceArea.svelte
@@ -6,9 +6,9 @@
 	<div
 		class="prose max-w-4xl min-h-fit mx-auto py-16 px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 lg:flex lg:items-center lg:justify-between"
 	>
-		<div class="w-full h-full">
-			<h2 class="text-3xl text-center font-extrabold tracking-tight">Service Area</h2>
-			<p class="mt-4 mb-12 text-center">
+		<div class="w-full h-full text-center">
+			<h2>Service Area</h2>
+			<p class="mt-4 mb-12">
 				We are proud to serve the South Puget Sound and parts of King County.
 			</p>
 			<div class="">
