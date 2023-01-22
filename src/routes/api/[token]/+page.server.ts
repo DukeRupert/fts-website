de4a501c384321a678184718a6906e43c7e77a04
Logan Williams <logan@firefly.llc>
2023-01-22 09:01:12 -0700

    Added success/failure page to unsubscribe function

---
+++ src/routes/api/[token]/+page.server.ts
@@ -0,0 +1,98 @@
+import { fail } from '@sveltejs/kit';
+import jwt from 'jsonwebtoken';
+import { PROMO_SECRET } from '$env/static/private';
+import type { PageServerLoad } from '../../[token]/$types';
+import { Supabase } from '$lib/db';
+import type { PromoModel } from '$lib/types/postmark';
+import Postmark from '$lib/postmarkClient';
+
+export interface Payload {
+	email: string;
+	iat: number;
+}
+
+const generateCode = (stringLength = 1) => {
+	let randomStr = '';
+	const characters =
+		'ABCDEFGHIJKLMNOPQRSTUVWXYZqeytrpolkadjsghfgmnbzxcvnQPOWEYRKASJHDGFMNBCVX--___-_jsfhrlg-_124903564576986483658fgh4sdfh687e4h897WETHJ68F7G4688471877GFHJFFGJ87469857468746hfghwrtiyj4598yhdjkhgnk';
+	for (let index = 0; index < stringLength; index++) {
+		randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return randomStr;
+};
+
+export const load: PageServerLoad = async ({ params }) => {
+	const { token } = params;
+
+	let validEmail = '';
+
+	try {
+		const decoded = jwt.verify(token, PROMO_SECRET);
+		if (decoded && typeof decoded !== 'string') {
+			const { email } = decoded as Payload;
+			validEmail = email;
+		}
+		if (decoded && typeof decoded === 'string') {
+			validEmail = decoded;
+		}
+	} catch (error) {
+		return {
+			success: false,
+			message: 'This token has expired.'
+		};
+	}
+
+	if (!validEmail) {
+		return { success: false, message: 'No valid email found.' };
+	}
+
+	// check if record exists and has not already been verified
+	const { data, error } = await Supabase.from('leads').select().eq('email', validEmail).single();
+
+	if (error) throw fail(400, { success: false, message: 'No record found for this email.' });
+
+	if (data?.verified === true) {
+		return { success: false, message: 'This email has already been validated.' };
+	}
+
+	// update record
+	const code = generateCode(5);
+	const { error: err } = await Supabase.from('leads')
+		.update({
+			verified: true,
+			verified_at: new Date().toUTCString(),
+			promo_waive_equipment_fee: code
+		})
+		.eq('email', validEmail);
+	if (err) return { success: false, message: 'Failure updating record' };
+
+	// Send email
+	const model: PromoModel = {
+		first_name: data?.first_name ?? 'Valued',
+		last_name: data?.last_name ?? 'Customer',
+		promo_value: '150',
+		promo_code: code,
+		action_url: 'www.fts-excavation.com/contact-us',
+		unsubscribe_url: `https://www.fts-excavation.com/api/unsubscribe/${token}`,
+		sender_name: 'FtS Customer Service',
+		company_name: 'FtS Excavation'
+	};
+
+	const res = await Postmark.sendEmailWithTemplate({
+		TemplateId: 30495956,
+		TemplateModel: model,
+		From: 'service@fts-excavation.com',
+		To: validEmail,
+		MessageStream: 'outbound',
+		TrackOpens: true
+	});
+
+	if (res.ErrorCode !== 0) {
+		return { success: false, message: 'An error occured when sending promo code email.' };
+	}
+
+	return {
+		success: true,
+		message: 'Your email has been verified.'
+	};
+};
---
+++ src/routes/api/unsubscribe/[token]/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="min-h-screen py-16 px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+	<div class="mx-auto max-w-max">
+		<main class="sm:flex flex-col">
+			{#if data?.success}
+				<div class="sm:ml-6">
+					<div class="sm:border-l sm:pl-6">
+						<h1 class="text-base font-semibold text-primary-500 tracking-wide uppercase">
+							Success
+						</h1>
+						<h2 class="mt-1 text-4xl font-extrabold sm:text-5xl sm:tracking-tight lg:text-6xl">
+							You have been unsubscribed.
+						</h2>
+					</div>
+				</div>
+			{:else}
+				<div class="sm:ml-6">
+					<div class="sm:border-l sm:pl-6">
+						<h1 class="text-base font-semibold text-primary-500 tracking-wide uppercase">Error</h1>
+						<h2 class="mt-1 text-4xl font-extrabold sm:text-5xl sm:tracking-tight lg:text-6xl">
+							{data?.message || 'Supplied token is invalid.'}
+						</h2>
+						<p class="max-w-xl mt-4 text-xl">
+							Please contact our technical support team <a href="mailto:service@firefly.llc">here</a
+							> if you have any further questions.
+						</p>
+					</div>
+				</div>
+			{/if}
+			<div class="mx-auto mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+				<a href="/" class="btn btn-filled-primary">Return Home</a>
+			</div>
+		</main>
+	</div>
+</div>
