2b57fae96d7766f1d9fd59592c8d0c9a97ce7b5a
Logan <logan@firefly.llc>
2023-02-09 12:52:17 -0700

    Reverted to adapter auto and removed edge functions

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({edge: true})
+		adapter: adapter()
 	}
 };
 
