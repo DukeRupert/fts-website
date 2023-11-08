1ae80dcebb0aa630e6233bc877cfceb1c73d886b
Logan <logan@fireflysoftware.dev>
2023-11-08 14:33:18 -0700

    Proof of concept for contacter plus api working

---
+++ src/routes/test/getLeads/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	console.log(data);
+</script>
+
+<h1>Leads</h1>
+
+<pre>{JSON.stringify(data, null, 2)}</pre>
