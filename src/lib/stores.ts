59cb0fd17134cdf373d1fb66ce64c9f64b33c139
Logan Williams <logan@fireflysoftware.dev>
2024-04-10 17:25:59 +0000

    Removed unused dependency and code for YouTube  integration.

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,4 @@
 import { writable } from 'svelte/store';
-import { asyncReadable, type Loadable } from '@square/svelte-store';
-import type { SearchListResponse, Video } from '$lib/types/youtube';
 
 // Navigation stores
 export const is_open = writable(false);
@@ -11,25 +9,3 @@ export const is_mobile_services_open = writable(false);
 // Contact form stores
 export const First_Name = writable('');
 export const Last_Name = writable('');
-
-// Youtube Channel Videos
-export const recentVideos: Loadable<Video[]> = asyncReadable(
-	[],
-	async () => {
-		const channelId = 'UC_LbjVWjiiFg3h9lB6rcvGQ';
-		const maxResults = 3;
-		const requestBody = JSON.stringify({ channelId: channelId, maxResults: maxResults });
-		const res = await fetch('/api/youtube', {
-			method: 'POST',
-			body: requestBody,
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		});
-		const data: SearchListResponse = await res.json();
-		const { items } = data;
-		const newItems = [...items];
-		return newItems;
-	},
-	{ reloadable: true }
-);
